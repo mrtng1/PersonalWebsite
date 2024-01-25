0add431af272507179856b0cc06244f764ffc476
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-01-25 13:45:02 +0100

    further animations and improvements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import {motion} from "framer-motion";
 
 function App() {
-  return (
-      <div className="main-container">
-        <div className="middle">
-            <p className="nameText">Martin Grulyo</p>
+    return (
+        <div className="main-container">
+            <motion.div className="middle" initial={{ y: '-8vh' }}
+                        animate={{ y: 0 }}
+                        transition={{ duration: 1.5 }}>
+                    <motion.p
+                        className="headerText"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 1 }}>
+
+                        Martin Grulyo
+                    </motion.p>
+            </motion.div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;

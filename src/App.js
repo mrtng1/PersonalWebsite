f97331aba2c9e62178e5ca59de115a71c6da7cee
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-01-25 15:41:59 +0100

    buttons contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import {motion} from "framer-motion";
+import { CiLinkedin } from "react-icons/ci";
+import {MdOutlineEmail} from "react-icons/md";
+import {IoIosArrowDown, IoLogoInstagram} from "react-icons/io";
 
 function App() {
+    const linkedinURL = "https://www.linkedin.com/in/mrtngrulyo/";
+    const email = "martingrulyo@gmail.com";
+    const instagramUrl = "https://www.instagram.com/martingrulyo/";
+
+    const handleLinkedInClick = () => {
+        window.location.href = linkedinURL;
+    };
+
+    const handleEmailClick = () => {
+        window.location.href = "mailto:" + email;
+    }
+
+    const handleInstagramClick = () => {
+        window.location.href = instagramUrl;
+    }
+
     return (
         <div className="main-container">
             <motion.div className="middle" initial={{ y: '-8vh' }}
                         animate={{ y: 0 }}
                         transition={{ duration: 1.5 }}>
+
+                <div>
                     <motion.p
                         className="headerText"
                         initial={{ opacity: 0 }}
@@ -15,6 +36,26 @@ function App() {
 
                         Martin Grulyo
                     </motion.p>
+
+                <div className="aboutMe">
+                    Computer Science student with skills in Java, TypeScript, JavaScript, Python, SQL, and GitHub.
+                    <br></br>
+                </div>
+
+                <div className="contactInfo">
+                    <button className="button" onClick={handleLinkedInClick}>
+                        <CiLinkedin />
+                    </button>
+
+                    <button className="button" onClick={handleEmailClick}>
+                        <MdOutlineEmail />
+                    </button>
+
+                    <button className="button" onClick={handleInstagramClick}>
+                        <IoLogoInstagram />
+                    </button>
+                </div>
+                </div>
             </motion.div>
         </div>
     );

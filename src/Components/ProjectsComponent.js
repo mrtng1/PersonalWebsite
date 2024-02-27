49e86987720c1bacc6e443646c52d20f340e8e4c
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-02-27 15:27:33 +0100

    design changes and skill card functionality

--- src/Components/ProjectsComponent.js
+++ src/Components/ProjectsComponent.js
@@ -3,57 +3,50 @@ import './Projects.css';
 import TechIcons from '../sources/image.shields.js';
 
 function ProjectsComponent() {
+
+    const projectsData = [
+        {
+            title: 'Craft Burger Web',
+            description: 'FullStack Web Application for displaying a menu from a restaurant, alongside a useful admin view for managing the whole website.',
+            repoLink: 'https://github.com/mrtng1/CraftBurger-Web',
+            techStack: [TechIcons.JAVASCRIPT, TechIcons.TYPESCRIPT, TechIcons.ANGULAR, TechIcons.SQL, TechIcons.CSHARP]
+        },
+        {
+            title: 'WUAV Documentation Software',
+            description: 'Desktop Application made in Java for creating and storing documentation for Audio/Visual Solutions. Winner of the Best Software in the CSe2022 class.',
+            repoLink: 'https://github.com/tomdra01/WUAV-Software',
+            techStack: [TechIcons.JAVA, TechIcons.SQL]
+        },
+        {
+            title: 'Movie Library',
+            description: 'Creating a simple, yet eye-catching UI with an API that retrieves posters for each movie from a sizeable database.',
+            repoLink: 'https://github.com/mrtng1/Movie-Library',
+            techStack: [TechIcons.JAVA, TechIcons.CSS, TechIcons.API]
+        },
+        {
+            title: '.NET Calculator',
+            description: 'A simple console program with a CI pipeline integration, SonarQube scanning and Drone CI, Docker integration.',
+            repoLink: 'https://github.com/mrtng1/CalculatorWithDb',
+            techStack: [TechIcons.CSHARP, TechIcons.DOCKER, TechIcons.SONARQUBE, TechIcons.DRONE_CI]
+        }
+    ];
+
     return (
         <div>
             <div className="subHeaderText disableUserSelect">My Projects</div>
             <div className="projects-grid">
-                <div className="project-card">
-                    <h3>Craft Burger Web</h3>
-                    <p>FullStack Web Application for displaying a menu from a restaurant, alongside a useful admin view for managing the whole website.</p>
-                    <a href="https://github.com/mrtng1/CraftBurger-Web" className="repo-button">Repository</a>
-                    <div className="tech-stack">
-                        <img src={TechIcons.JAVASCRIPT} alt="JavaScript" className="tech-icon"/>
-                        <img src={TechIcons.TYPESCRIPT} alt="TypeScript" className="tech-icon"/>
-                        <img src={TechIcons.ANGULAR} alt="Angular" className="tech-icon"/>
-                        <img src={TechIcons.SQL} alt="SQL" />
-                        <img src={TechIcons.CSHARP} alt="C#" />
+                {projectsData.map((project, index) => (
+                    <div className="project-card" key={index}>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <a href={project.repoLink} className="repo-button">Repository</a>
+                        <div className="tech-stack">
+                            {project.techStack.map((tech, techIndex) => (
+                                <img key={techIndex} src={tech} alt={tech.split('/').pop().split('.')[0]} className="tech-icon" />
+                            ))}
+                        </div>
                     </div>
-                </div>
-
-                <div className="project-card">
-                    <h3>WUAV Documentation Software</h3>
-                    <p>Desktop Application made in Java for creating and storing documentation for Audio/Visual Solutions. Winner of the Best Software in the CSe2022 class.</p>
-                    <a href="https://github.com/tomdra01/WUAV-Software" className="repo-button">Repository</a>
-                    <div className="tech-stack">
-                        <img src={TechIcons.JAVA} alt="Java" />
-                        <img src={TechIcons.SQL} alt="SQL" />
-                    </div>
-                </div>
-
-                <div className="project-card">
-                    <h3>Movie Library</h3>
-                    <p>Creating a simple, yet eye-catching UI with an API that retrieves posters for each movie from a sizeable database.</p>
-                    <a href="https://github.com/mrtng1/Movie-Library" className="repo-button">Repository</a>
-                    <div className="tech-stack">
-                        <img src={TechIcons.JAVA} alt="Java" />
-                        <img src={TechIcons.CSS} alt="CSS" />
-                        <img src={TechIcons.API} alt="API" />
-
-                    </div>
-                </div>
-
-                <div className="project-card">
-                    <h3>.NET Calculator</h3>
-                    <p>A simple console program with a CI pipeline integration, SonarQube scanning and Drone CI, Docker integration.</p>
-                    <a href="https://github.com/mrtng1/CalculatorWithDb" className="repo-button">Repository</a>
-                    <div className="tech-stack">
-                        <img src={TechIcons.CSHARP} alt="C#" />
-                        <img src={TechIcons.DOCKER} alt="Docker" />
-                        <img src={TechIcons.SONARQUBE} alt="SonarQube" />
-                        <img src={TechIcons.DRONE_CI} alt="Drone CI" />
-                    </div>
-                </div>
-
+                ))}
             </div>
         </div>
     );
--- src/Components/SkillsComponent.js
+++ src/Components/SkillsComponent.js
@@ -4,108 +4,112 @@ import '.././App.css';
 import TechIcons from "../sources/image.shields";
 
 function SkillsComponent() {
+    const navigateTo = (url) => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
+
     return (
         <div>
             <div className="subHeaderText disableUserSelect">My Skills</div>
             <div className="skillsGrid">
                 {/* JavaScript */}
-                <div className="skillItem">
-                    <img src={TechIcons.JAVASCRIPT} alt="JavaScript" />
+                <div className="skillItem" onClick={() => navigateTo('https://www.javascript.com')}>
+                    <img src={TechIcons.JAVASCRIPT} alt="JavaScript"/>
                     JavaScript
                 </div>
                 {/* TypeScript */}
-                <div className="skillItem">
-                    <img src={TechIcons.TYPESCRIPT} alt="TypeScript" />
+                <div className="skillItem" onClick={() => navigateTo('https://www.typescriptlang.org/')}>
+                    <img src={TechIcons.TYPESCRIPT} alt="TypeScript"/>
                     TypeScript
                 </div>
                 {/* C# */}
-                <div className="skillItem">
+                <div className="skillItem" onClick={() => navigateTo("https://dotnet.microsoft.com/en-us/languages/csharp")}>
                     <img src={TechIcons.CSHARP} alt="C#" />
                     C#
                 </div>
                 {/*.NET */}
-                <div className="skillItem">
+                <div className="skillItem" onClick={() => navigateTo("https://dotnet.microsoft.com/en-us/")}>
                     <img src={TechIcons.DOTNET} alt=".NET" />
                     .NET
                 </div>
                 {/* Java */}
-                <div className="skillItem">
-                    <img src={TechIcons.JAVA} alt="Java" />
+                <div className="skillItem" onClick={() => navigateTo("https://dev.java/")}>
+                    <img src={TechIcons.JAVA} alt="Java"/>
                     Java
                 </div>
                 {/* Python */}
-                <div className="skillItem">
-                    <img src={TechIcons.PYTHON} alt="Python" />
+                <div className="skillItem" onClick={() => navigateTo("https://www.python.org/")}>
+                    <img src={TechIcons.PYTHON} alt="Python"/>
                     Python
                 </div>
                 {/* Docker */}
-                <div className="skillItem">
-                    <img src={TechIcons.DOCKER} alt="Docker" />
+                <div className="skillItem" onClick={() => navigateTo("https://www.docker.com/")}>
+                    <img src={TechIcons.DOCKER} alt="Docker"/>
                     Docker
                 </div>
                 {/* SonarQube */}
-                <div className="skillItem">
-                    <img src={TechIcons.SONARQUBE} alt="SonarQube" />
+                <div className="skillItem" onClick={() => navigateTo("https://www.sonarsource.com/products/sonarqube/")}>
+                    <img src={TechIcons.SONARQUBE} alt="SonarQube"/>
                     SonarQube
                 </div>
                 {/* Drone CI */}
-                <div className="skillItem">
-                    <img src={TechIcons.DRONE_CI} alt="Drone CI" />
+                <div className="skillItem" onClick={() => navigateTo("https://www.drone.io/")}>
+                    <img src={TechIcons.DRONE_CI} alt="Drone CI"/>
                     Drone CI
                 </div>
                 {/* Kali Linux */}
-                <div className="skillItem">
-                    <img src={TechIcons.KALI_LINUX} alt="Kali Linux" />
+                <div className="skillItem" onClick={() => navigateTo("https://www.kali.org/")}>
+                    <img src={TechIcons.KALI_LINUX} alt="Kali Linux"/>
                     Kali Linux
                 </div>
                 {/*Ubuntu */}
-                <div className="skillItem">
-                    <img src={TechIcons.UBUNTU} alt="Ubuntu" />
+                <div className="skillItem" onClick={() => navigateTo("https://ubuntu.com/")}>
+                    <img src={TechIcons.UBUNTU} alt="Ubuntu"/>
                     Ubuntu
                 </div>
                 {/* K6 Testing */}
-                <div className="skillItem">
-                    <img src={TechIcons.K6_TESTING} alt="K6 Testing" />
+                <div className="skillItem" onClick={() => navigateTo("https://k6.io/")}>
+                    <img src={TechIcons.K6_TESTING} alt="K6 Testing"/>
                     K6 Testing
                 </div>
                 {/* Unit Testing */}
-                <div className="skillItem">
-                    <img src={TechIcons.UNIT_TESTING} alt="Unit Testing" />
+                <div className="skillItem" onClick={() => navigateTo("https://aws.amazon.com/what-is/unit-testing/")}>
+                    <img src={TechIcons.UNIT_TESTING} alt="Unit Testing"/>
                     Unit Testing
                 </div>
                 {/* SQL */}
-                <div className="skillItem">
-                    <img src={TechIcons.SQL} alt="SQL" />
+                <div className="skillItem" onClick={() => navigateTo("https://aws.amazon.com/what-is/sql/")}>
+                    <img src={TechIcons.SQL} alt="SQL"/>
                     SQL
                 </div>
                 {/* Git */}
-                <div className="skillItem">
-                    <img src={TechIcons.GIT} alt="Git" />
+                <div className="skillItem" onClick={() => navigateTo("https://git-scm.com/")}>
+                    <img src={TechIcons.GIT} alt="Git"/>
                     Git
                 </div>
                 {/*Rest API */}
-                <div className="skillItem">
-                    <img src={TechIcons.API} alt="Rest API" />
+                <div className="skillItem" onClick={() => navigateTo("https://www.ibm.com/topics/rest-apis")}>
+                    <img src={TechIcons.API} alt="Rest API"/>
                     Rest API
                 </div>
                 {/* Angular */}
-                <div className="skillItem">
-                    <img src={TechIcons.ANGULAR} alt="Angular" />
+                <div className="skillItem" onClick={() => navigateTo("https://angular.dev/")}>
+                    <img src={TechIcons.ANGULAR} alt="Angular"/>
                     Angular
                 </div>
                 {/* React */}
-                <div className="skillItem">
-                    <img src={TechIcons.REACT} alt="React" />
+                <div className="skillItem" onClick={() => navigateTo("https://react.dev/")}>
+                    <img src={TechIcons.REACT} alt="React"/>
                     React
                 </div>
                 {/*Blazor */}
-                <div className="skillItem">
-                    <img src={TechIcons.BLAZOR} alt="Blazor" />
+                <div className="skillItem" onClick={() => navigateTo("https://learn.microsoft.com/en-us/aspnet/core/blazor/?view=aspnetcore-8.0")}>
+                    <img src={TechIcons.BLAZOR} alt="Blazor"/>
                     Blazor
                 </div>
                 {/*Scrum */}
-                <div className="skillItem">
-                    <img src={TechIcons.SCRUM} alt="Scrum" />
+                <div className="skillItem" onClick={() => navigateTo("https://www.scrum.org/resources/what-scrum-module")}>
+                    <img src={TechIcons.SCRUM} alt="Scrum"/>
                     Scrum
                 </div>
             </div>

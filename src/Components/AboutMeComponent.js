b91e5545f65eade129c2c10c69e162b41e3b4856
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2024-02-27 02:19:05 +0100

    AboutMe changes

--- src/Components/AboutMeComponent.js
+++ src/Components/AboutMeComponent.js
@@ -4,13 +4,14 @@ import '.././App.css';
 function AboutMeComponent() {
     return (
         <div>
-            <div class="subHeaderText disableUserSelect">About Me</div>
-            <div class="basicText">
-                Computer Science Student, I specialize in Java, TypeScript, JavaScript, Python, SQL, and GitHub, enriched by a Cisco CCNA certification for network skills. <br />
-                Expanding into C# and web development with React & Angular, I balance multiple projects with proven leadership in team environments. <br />
-                My core strengths lie in communication, teamwork, and problem-solving, with a passion for enhancing web security and incorporating agile methodologies into secure web development. <br />
-                Eager to connect and contribute to the technology community, I'm on a quest for continuous learning and innovation in web technologies. <br />
-            </div>
+            <div className="subHeaderText disableUserSelect">About Me</div>
+            <ul className="basicText">
+                <li>Computer Science Student from Bratislava, Slovakia.</li> <br></br>
+                <li>Specialize in Back-End and Front-End Development; studying DevOps and Cyber-security, with CCNA1 certification.</li> <br></br>
+                <li>Expanding skills into C# and web development with React & Angular, balancing multiple projects with proven leadership in team environments.</li> <br></br>
+                <li>Strengths: dedication, reliability, teamwork, and commitment to high-quality software with a focus on web security and agile practices.</li> <br></br>
+                <li>Eager to connect, contribute, and continuously learn and innovate in web technologies.</li> <br></br>
+            </ul>
         </div>
     );
 }
